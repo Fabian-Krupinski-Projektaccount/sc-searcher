6ab63a87b5a21d65f6100f369f314bfff97b74c1
Fabian-Krupinski-Projektaccount <github.hdfavo@gmail.com>
2020-11-14 18:11:33 +0100

    .

--- Search.js
+++ Search.js
@@ -1,14 +1,12 @@
 const request = require('request');
 
-var searchURL = 'https://api-v2.soundcloud.com/search/tracks?q=SEARCH_TERM&client_id=CLIENT_ID&limit=LIMIT_TERM';
-
 exports.getTracks = function (client_id, search, limit, callback) {
     if(typeof search != 'string') throw "Seach term is not type of string";
     if(isNaN(limit)) throw "Not a number";
     if(limit > 100 || limit < 1) throw "Limit must be between 1 and 100";
     if(!client_id) throw "Must have a client id";
 
-    var searchURL = 'https://api-v2.soundcloud.com/search/tracks?q=SEARCH_TERM&client_id=' + client_id + '&limit=LIMIT_TERM';
+    var searchURL = 'https://api-v2.soundcloud.com/search/tracks?q=SEARCH_TERM&client_id=CLIENT_ID&limit=LIMIT_TERM';;
 
     var spaces = search.split(" ");
     for(var i=0; i < spaces.length; i++){
