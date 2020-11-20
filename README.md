cad6b95c7ace077bdd582edbb3639bcd6c6a91a1
Fabian-Krupinski-Projektaccount <github.hdfavo@gmail.com>
2020-11-20 20:02:41 +0100

    .

--- README.md
+++ README.md
@@ -7,19 +7,20 @@
 
 **Examples**
 
-    const scSearch = require('sc-searcher');
+    const ScSearcher = require('sc-searcher');
+    const scSearch = new ScSearcher();
 
-    var client_id = 'egDzE3xmafwb5ki9VMBsIXdrtAEmZPtq';
-    var query = 'Query';
+
+    var client_id = process.env.CLIENT_ID;
+    var query = 'Avicii';
     var result_limit = 5;
 
     scSearch.init(client_id);
 
-    (async () => {
-        var results = await scSearch.getTracks(query, result_limit);
-
-        console.log(results);
-    })();
+    scSearch.getTracks(query, result_limit)
+        .then((res) => {
+            console.log(res);
+        });
 
 
 
--- Search.js
+++ Search.js
@@ -1,45 +1,50 @@
 const request = require('request');
 
-var client_id;
+class ScSearcher {
+    constructor() {
+        this.client_id = null;
+    }
 
-exports.init = (clientId) => {
-    if (!clientId) return;
-    client_id = clientId;
-}
+    init(client_id) {
+        this.client_id = client_id;
+    }
 
-exports.getTracks = function(search, limit) {
-    return new Promise((res, rej) => {
-        if(typeof search != 'string') throw 'Seach term is not type of string';
-        if(isNaN(limit)) throw 'Not a number';
-        if(limit > 100 || limit < 1) throw 'Limit must be between 1 and 100';
-        if(!client_id) throw 'Must set client id with init() first';
+    getTracks(search, limit, callback) {
+        return new Promise((res, rej) => {
+            if(typeof search != 'string') throw 'Seach term is not type of string';
+            if(isNaN(limit)) throw 'Not a number';
+            if(limit > 100 || limit < 1) throw 'Limit must be between 1 and 100';
+            if(!this.client_id) throw 'Must set client id with init() first';
 
-        var searchURL = 'https://api-v2.soundcloud.com/search/tracks?q=SEARCH_TERM&client_id=CLIENT_ID_TERM&limit=LIMIT_TERM';;
+            var searchURL = 'https://api-v2.soundcloud.com/search/tracks?q=SEARCH_TERM&client_id=CLIENT_ID_TERM&limit=LIMIT_TERM';;
 
-        var spaces = search.split(' ');
-        for(var i=0; i < spaces.length; i++) {
-            search = search.replace(' ', '%20');
-        }
+            var spaces = search.split(' ');
+            for(var i=0; i < spaces.length; i++) {
+                search = search.replace(' ', '%20');
+            }
 
-        searchURL = searchURL.replace('CLIENT_ID_TERM', client_id).replace('SEARCH_TERM', search).replace('LIMIT_TERM', limit);
+            searchURL = searchURL.replace('CLIENT_ID_TERM', this.client_id).replace('SEARCH_TERM', search).replace('LIMIT_TERM', limit);
 
-        const promise = new Promise((_res, _rej) =>
-            request({url: searchURL}, function (error, response, body) {
-                if(error) return _rej(new Error(error));
+            const promise = new Promise((_res, _rej) =>
+                request({url: searchURL}, function (error, response, body) {
+                    if(error) return _rej(new Error(error));
 
-                if (!error && response.statusCode === 200){
-                    return _res({
-                        body: JSON.parse(body)
-                    });
-                }
-            })
-        );
+                    if (!error && response.statusCode === 200){
+                        return _res({
+                            body: JSON.parse(body)
+                        });
+                    }
+                })
+            );
 
-        promise.then(response => {
-            const body = response.body;
-            const track_list = body.collection;
+            promise.then(response => {
+                const body = response.body;
+                const track_list = body.collection;
 
-            return res(track_list);
+                return res(track_list);
+            });
         });
-    });
+    }
 }
+
+module.exports = ScSearcher;
